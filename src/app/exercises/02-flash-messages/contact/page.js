05d2920037518ba0d642e8f12b130c3f1f7b709e
olhanotolga <olha.halat@gmail.com>
2025-11-10 18:37:19 +0100

    feat: redirect user to contact page upon submitting form

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,18 @@
 'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation'
 
 function ContactPage() {
+  const router = useRouter();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    router.push('/exercises/02-flash-messages');
+  }
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
